e48ec61de5f031419b5cb96ff5f4723f6b820c0f
tanjiancheng <448755877@qq.com>
2014-04-25 20:56:22 +0800

    Create server.php

---
+++ server.php
@@ -0,0 +1,27 @@
+<?php
+set_time_limit(0);
+$filename = dirname(__FILE__).'/data.txt';
+
+$msg = isset($_GET['msg']) ? $_GET['msg'] : ''; 
+if ($msg != '') 
+{ 
+	file_put_contents($filename,$msg); 
+	die(); 
+}
+
+$lastmodif = isset($_GET['timestamp']) ? $_GET['timestamp'] : 0; 
+$currentmodif = filemtime($filename);
+while ($currentmodif <= $lastmodif) // check if the data file has been modified 
+{ 
+	usleep(10000); // sleep 10ms to unload the CPU 
+	clearstatcache(); 
+	$currentmodif = filemtime($filename); 
+}
+
+$response = array(); 
+$response['msg'] = file_get_contents($filename); 
+$response['msg'].= $lastmodif ."|". $currentmodif;
+$response['timestamp'] = $currentmodif; 
+echo json_encode($response); 
+flush();
+?>
