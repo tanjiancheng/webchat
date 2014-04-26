ccaad1f4194e61c04959f5b85de6acb5a1580338
tanjiancheng <tanjianchengcc@gmail.com>
2014-04-26 11:53:09 +0800

    all strust change

--- server.php
+++ server.php
@@ -1,16 +1,22 @@
 <?php
+
+/**
+ * 
+ */
+
 set_time_limit(0);
 $filename = dirname(__FILE__).'/data.txt';
 
 $msg = isset($_GET['msg']) ? $_GET['msg'] : ''; 
-if ($msg != '') 
-{ 
+if ($msg != '') { 
 	file_put_contents($filename,$msg); 
 	die(); 
 }
 
 $lastmodif = isset($_GET['timestamp']) ? $_GET['timestamp'] : 0; 
+
 $currentmodif = filemtime($filename);
+
 while ($currentmodif <= $lastmodif) // check if the data file has been modified 
 { 
 	usleep(10000); // sleep 10ms to unload the CPU 
